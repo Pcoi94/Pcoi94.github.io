deac3e33d08b1e6ec80033d5fc5384484eb17208
Pcoi94 <107702714+Pcoi94@users.noreply.github.com>
2023-10-01 13:43:55 +0200

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+document.getElementById("youtube-button").addEventListener("click", function() {
+    window.open('https://www.youtube.com/@pcoi94')
+});
+
+document.getElementById("roblox-button").addEventListener("click", function() {
+    window.open("https://www.roblox.com/users/521059478/profile");
+});
+
+document.getElementById("carrd-button").addEventListener("click", function() {
+    window.open('https://pcoiscript.carrd.co/#home')
+});
